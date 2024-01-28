d67a00694b66ad494bb725c0f5d7b394fac98141
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2024-01-27 23:53:52 -0300

    Add files via upload

--- rotulos_ggplot2.R
+++ rotulos_ggplot2.R
@@ -11,6 +11,18 @@ library(tidyverse)
 
 ## Carregar dados
 
-dados <- dados::mtcarros
+view(dados::mtcarros)
 
+## Selecionar as 30 primeiras linhas
+
+dados = head(mtcarros, 30)
 view(dados)
+ 
+# 1/ add text with geom_text, use nudge to nudge the text
+ggplot(data, aes(x=wt, y=mpg)) +
+  geom_point() + # Show dots
+  geom_text(
+    label=rownames(data), 
+    nudge_x = 0.25, nudge_y = 0.25, 
+    check_overlap = T
+  )
